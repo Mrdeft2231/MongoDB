35f5bee16450ba3f4836a34c5cd6194ae7881cae
Mrdeft2231 <116429749+Mrdeft2231@users.noreply.github.com>
2024-05-09 15:52:08 +0300

    add

--- backend-connect-db/controllers/auth.js
+++ backend-connect-db/controllers/auth.js
@@ -4,21 +4,8 @@ const users = require("../models/user.js");
 const login = (req, res) => {
   const { email, password } = req.body;
 
-  users.findOne({ email })
-  .then(user => {
-    if (!user) {
+  users.findUserByCredentials('emailExample@yandex.ru', 'passwordExample')
 
-      return Promise.reject(new Error("Неправильная почта или пароль"));
-
-    }
-    return bcrypt.compare( password, user.password ).then(matched => {
-      if (!matched) {
-        return Promise.reject(new Error("Неправильная почта или пароль"));
-      }
-      return user;
-    })
-  
-  })
   .then((user) => {
     res.status(200).send({ _id: user._id, username: user.username, email: user.email })
   })
